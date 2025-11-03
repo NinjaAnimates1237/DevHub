04881e1a3afd58bb1b5e57cc0f77c8e494c57eed
Lenny Yohannan <lennyyohannan@mac.lan>
2025-11-02 19:16:56 -0800

    Update DevHub with new server and username features

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static('public')); // serve HTML, CSS, JS
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+// Track servers and users
+let servers = {}; // { serverName: [username1, username2] }
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('User connected:', socket.id);
 
-  socket.on('chat message', (msg) => io.emit('chat message', msg));
-  socket.on('typing', (username) => socket.broadcast.emit('typing', username));
-  socket.on('stop typing', () => socket.broadcast.emit('stop typing'));
+  socket.on('join-server', ({ serverName, username }) => {
+    socket.join(serverName);
 
-  socket.on('disconnect', () => console.log('A user disconnected'));
+    if (!servers[serverName]) servers[serverName] = [];
+    if (!servers[serverName].includes(username)) servers[serverName].push(username);
+
+    // Notify server users
+    io.to(serverName).emit('server-message', `${username} joined ${serverName}`);
+    
+    // Update everyone with server list
+    io.emit('update-servers', servers);
+  });
+
+  socket.on('chat-message', ({ serverName, username, message }) => {
+    io.to(serverName).emit('chat-message', { username, message });
+  });
+
+  socket.on('disconnecting', () => {
+    // Remove user from all servers they were in
+    for (const room of socket.rooms) {
+      if (servers[room]) {
+        servers[room] = servers[room].filter(u => u !== socket.id);
+      }
+    }
+    io.emit('update-servers', servers);
+  });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(\`Server running on port \${PORT}\`));
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
