854d3601e7eb6bdc294c702264ed679c785ecaa3
NinjaAnimates1237 <adrianjoji5@gmail.com>
2025-11-06 18:43:21 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,79 +1,58 @@
+// server.js (replace old Socket.IO logic with this)
+
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
-let users = {}; // socket.id -> username
-let admins = new Set(["NinjaIsCool"]);
-let banned = new Set();
+const users = {}; // { socketId: username }
+const admins = new Set(["NinjaIsCool"]); // Default admin
 
 io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+  console.log("New connection:", socket.id);
 
   socket.on("setUsername", (username) => {
-    if (banned.has(username)) {
-      socket.emit("banned");
-      socket.disconnect();
-      return;
-    }
-
     users[socket.id] = username;
-
-    // Admin check
-    if (admins.has(username)) {
-      socket.emit("adminStatus", true);
-    } else {
-      socket.emit("adminStatus", false);
-    }
-
-    io.emit("userList", Object.values(users));
-    console.log(`${username} joined`);
+    io.emit("chatMessage", { user: "System", msg: `${username} joined the chat.` });
   });
 
-  socket.on("sendMessage", (data) => {
-    const username = users[socket.id];
-    if (!username) return;
-
-    io.emit("newMessage", { username, text: data });
+  socket.on("chatMessage", (msg) => {
+    const user = users[socket.id];
+    if (!user) return;
+    io.emit("chatMessage", { user, msg });
   });
 
-  // ✅ Admin banning
-  socket.on("banUser", (target) => {
-    const username = users[socket.id];
-    if (!admins.has(username)) return;
-
-    const targetSocket = Object.keys(users).find((id) => users[id] === target);
-    if (targetSocket) {
-      banned.add(target);
-      io.to(targetSocket).emit("banned");
-      io.sockets.sockets.get(targetSocket).disconnect();
-      io.emit("serverNotification", `${target} was banned by ${username}`);
-    }
-  });
-
-  // ✅ Give admin
   socket.on("makeAdmin", (target) => {
-    const username = users[socket.id];
-    if (!admins.has(username)) return;
+    const requester = users[socket.id];
+    if (admins.has(requester)) {
+      admins.add(target);
+      io.emit("notification", { msg: `${target} was made an admin by ${requester}` });
+    } else {
+      socket.emit("notification", { msg: "You are not authorized to do that." });
+    }
+  });
 
-    admins.add(target);
-    io.emit("serverNotification", `${target} is now an admin!`);
+  socket.on("banUser", (target) => {
+    const requester = users[socket.id];
+    if (admins.has(requester)) {
+      const targetSocket = Object.keys(users).find((id) => users[id] === target);
+      if (targetSocket) {
+        io.to(targetSocket).emit("banned");
+        io.emit("notification", { msg: `${target} was banned by ${requester}` });
+      }
+    }
   });
 
   socket.on("disconnect", () => {
     const username = users[socket.id];
-    if (username) {
-      delete users[socket.id];
-      io.emit("userList", Object.values(users));
-      console.log(`${username} disconnected`);
-    }
+    delete users[socket.id];
+    io.emit("chatMessage", { user: "System", msg: `${username} left.` });
   });
 });
 
-server.listen(3000, () => console.log("✅ Server running on port 3000"));
+server.listen(3000, () => console.log("Server running on port 3000"));
